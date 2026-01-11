e896d97889949098f04ff439150583b0861d3c1e
kaustubhduse <202251045@iiitvadodara.ac.in>
2026-01-11 16:44:54 +0530

    Added to vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,20 @@
-import "./globals.css"
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 text-gray-900" suppressHydrationWarning={true}>
+      <body
+        className="bg-gray-100 text-gray-900"
+        suppressHydrationWarning={true}
+      >
         {children}
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
